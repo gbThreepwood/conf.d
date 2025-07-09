fa02b5d430bd06feb6452367237edda4046ce5d9
Eirik Haustveit <eirik@la2kta.net>
2025-07-09 21:56:41 +0200

    Updated emacs config

--- emacs/emacs-custom.el
+++ emacs/emacs-custom.el
@@ -3,12 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(conda-anaconda-home "/usr/bin/conda/")
  '(custom-enabled-themes '(spacemacs-dark))
  '(custom-safe-themes
    '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" default))
  '(package-selected-packages
-   '(expand-region multiple-cursors mark-multiple swiper citar kbd-mode treemacs-tab-bar treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs sublimity-scroll sublimity linum-relative magit helm ob-spice spice-mode flycheck yasnippet-snippets yasnippet projectile browse-kill-ring popup-kill-ring exwm symon dmenu diminish spaceline company dashboard rainbow-delimiters sudo-edit hungry-delete switch-window rainbow-mode avy smex ido-vertical-mode org-bullets beacon spacemacs-theme which-key use-package))
- '(package-vc-selected-packages '((kbd-mode :url "https://github.com/kmonad/kbd-mode"))))
+   '(hide-mode-line org-tree-slide crux pdf-tools perspective windresize org-re-reveal ox-reveal visual-fill-column org-present jupyter elpy auctex markdown-mode sphinx-mode org-super-agenda platformio-mode format-all expand-region multiple-cursors mark-multiple swiper citar kbd-mode treemacs-tab-bar treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs sublimity-scroll sublimity linum-relative magit helm ob-spice spice-mode flycheck yasnippet-snippets yasnippet projectile browse-kill-ring popup-kill-ring exwm symon dmenu diminish spaceline company dashboard rainbow-delimiters sudo-edit hungry-delete switch-window rainbow-mode avy smex ido-vertical-mode org-bullets beacon spacemacs-theme which-key use-package))
+ '(package-vc-selected-packages
+   '((kbd-mode :vc-backend Git :url "https://www.github.com/kmonad/kbd-mode"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
